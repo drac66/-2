23c62acb7cd487a4dbe577f8c777d5a0b3638ca2
damaster <1037121237@qq.com>
2026-04-24 19:31:41 +0800

    feat(wxcloud): add subtle tap ripple and tiny star burst click effect on home actions

--- wxcloud-version/miniprogram/pages/home/home.js
+++ wxcloud-version/miniprogram/pages/home/home.js
@@ -1,4 +1,8 @@
 Page({
+  data: {
+    clickFx: []
+  },
+
   onShow() {
     const token = wx.getStorageSync('token');
     if (!token) {
@@ -6,17 +10,40 @@ Page({
     }
   },
 
-  toAlbum() { wx.navigateTo({ url: '/pages/album/album' }); },
-  toMessages() { wx.navigateTo({ url: '/pages/messages/messages' }); },
-  toDiary() { wx.navigateTo({ url: '/pages/diary/diary' }); },
+  spawnClickFx(e) {
+    const x = (e && e.detail && typeof e.detail.x === 'number') ? e.detail.x : 0;
+    const y = (e && e.detail && typeof e.detail.y === 'number') ? e.detail.y : 0;
+    const id = `${Date.now()}_${Math.floor(Math.random() * 10000)}`;
+    const fx = { id, x, y };
 
-  logout() {
+    this.setData({ clickFx: [...this.data.clickFx, fx] });
+    setTimeout(() => {
+      this.setData({ clickFx: this.data.clickFx.filter((it) => it.id !== id) });
+    }, 520);
+  },
+
+  goWithFx(e, url) {
+    this.spawnClickFx(e);
+    setTimeout(() => {
+      wx.navigateTo({ url });
+    }, 120);
+  },
+
+  toAlbum(e) { this.goWithFx(e, '/pages/album/album'); },
+  toMessages(e) { this.goWithFx(e, '/pages/messages/messages'); },
+  toDiary(e) { this.goWithFx(e, '/pages/diary/diary'); },
+  toSettings(e) { this.goWithFx(e, '/pages/settings/settings'); },
+
+  logout(e) {
+    this.spawnClickFx(e);
     wx.removeStorageSync('token');
     wx.removeStorageSync('openid');
+    wx.removeStorageSync('nickname');
 
     const app = getApp();
     app.globalData.token = '';
     app.globalData.openid = '';
+    app.globalData.nickname = '';
 
     wx.showToast({ title: '已退出登录', icon: 'success' });
     setTimeout(() => {
